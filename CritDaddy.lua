7f5c3e6492d2a53885eab264d391f382c6d5f6ee
sbowers.sequoia@gmail.com <sbowers.sequoia@gmail.com>
2024-01-05 14:27:10 -0600

    shuffling filenames again, confirmed random sound config is functional with new soundfiles

--- CritDaddy.lua
+++ CritDaddy.lua
@@ -10,13 +10,12 @@ local CritDaddy = AceAddon:NewAddon("CritDaddy", "AceConsole-3.0", "AceEvent-3.0
 -- Define the sounds directory and list of sound files
 local SOUND_DIR = "Interface\\AddOns\\CritDaddy\\sounds"
 local soundFiles = {
-    "kaiwow0.wav",
-    "kaiwow1.wav",
+    "kaiwow.wav",
     "oohwhee.wav",
     "potofgreed.mp3",
     "theefiecook.mp3",
-    "tiocca0.wav",
-    "tiocca1.wav",
+    "tioccawow.wav",
+    "tioccacrit.wav",
     -- Add more sound files as needed
 }
 
